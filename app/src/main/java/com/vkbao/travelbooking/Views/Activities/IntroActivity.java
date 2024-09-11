145a45a18deae5e703a00fd7f15385e78eefaef8
baovkb <21521860@gm.uit.edu.vn>
2024-09-11 08:21:46 +0700

    avoid intro after the first open

--- app/src/main/java/com/vkbao/travelbooking/Views/Activities/IntroActivity.java
+++ app/src/main/java/com/vkbao/travelbooking/Views/Activities/IntroActivity.java
@@ -1,6 +1,7 @@
 package com.vkbao.travelbooking.Views.Activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -12,11 +13,23 @@ public class IntroActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("intro", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        boolean isFirst = sharedPreferences.getBoolean("first", true);
+        if (!isFirst) {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        } else {
+            editor.putBoolean("first", false);
+            editor.apply();
+        }
+
         binding = ActivityIntroBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         binding.introBtn.setOnClickListener((view -> {
-            Log.d("test", "intro is clicked");
             startActivity(new Intent(this, MainActivity.class));
             finish();
         }));
